0e7f8422c51739d6474ef9c5d89683129639dc1f
Nguyen Tung Lam <ntlamctu@gmail.com>
2022-09-29 14:17:30 +0700

    Create project for DTOs

---
+++ TheSkyHomestay.DTO/RoomCategory/RoomCategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSkyHomestay.DTO.RoomCategory
+{
+    public class RoomCategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+    }
+}
