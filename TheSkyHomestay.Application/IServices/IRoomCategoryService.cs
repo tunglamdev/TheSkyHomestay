f638a9c303b5860f601ca8fdc141ae4b847c2ebd
Nguyen Tung Lam <ntlamctu@gmail.com>
2022-09-29 14:17:51 +0700

    Create project for services

---
+++ TheSkyHomestay.Application/IServices/IRoomCategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSkyHomestay.DTO.RoomCategory;
+
+namespace TheSkyHomestay.Application.IServices
+{
+    public interface IRoomCategoryService
+    {
+        public Task<List<RoomCategoryDTO>> GetAllAsync();
+        public Task<RoomCategoryDTO> GetByIdAsync(int Id);
+        //public Task<int> CreateAsync(CategoryCreateRequest request);
+        //public Task<string> UpdateAsync(CategoryUpdateRequest request);
+        //public Task<int> DeleteAsync(int Id);
+    }
+}
---
+++ TheSkyHomestay.Application/Services/RoomCategoryService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSkyHomestay.Application.IServices;
+using TheSkyHomestay.Data.EF;
+using TheSkyHomestay.DTO.RoomCategory;
+
+namespace TheSkyHomestay.Application.Services
+{
+    public class RoomCategoryService : IRoomCategoryService
+    {
+        private readonly TheSkyHomestayDbContext _context;
+        public RoomCategoryService(TheSkyHomestayDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<RoomCategoryDTO>> GetAllAsync()
+        {
+            return await _context.RoomCategories.Where(rc => rc.IsDeleted == false).Select(rc => new RoomCategoryDTO()
+            {
+                Id = rc.Id,
+                Name = rc.Name,
+                CreatedTime = rc.CreatedTime,
+                UpdatedTime = rc.UpdatedTime
+             }).ToListAsync();
+        }
+
+        public async Task<RoomCategoryDTO> GetByIdAsync(int Id)
+        {
+            return await _context.RoomCategories.Where(rc => rc.IsDeleted == false && rc.Id==Id).Select(rc => new RoomCategoryDTO()
+            {
+                Id = rc.Id,
+                Name = rc.Name,
+                CreatedTime = rc.CreatedTime,
+                UpdatedTime = rc.UpdatedTime
+            }).FirstOrDefaultAsync();
+        }
+    }
+}
