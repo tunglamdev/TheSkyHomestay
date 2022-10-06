9614fc2fa5311c47f91314ea4e80ea7b612d9f58
Nguyen Tung Lam <ntlamctu@gmail.com>
2022-10-06 16:27:08 +0700

    Create DTO ApiResut

---
+++ TheSkyHomestay.DTO/Contants/ApiResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSkyHomestay.DTO.Contants
+{
+    public class ApiResult<T>
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public T? Data { get; set; }
+
+        public ApiResult(T? resultData)
+        {
+            Data = resultData;
+        }
+    }
+}
