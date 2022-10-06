56be48c48c168243d092eb52d16e9c3731dca973
Nguyen Tung Lam <ntlamctu@gmail.com>
2022-10-06 16:32:12 +0700

    Update Bill Model

--- TheSkyHomestay.Data/Models/Bill.cs
+++ TheSkyHomestay.Data/Models/Bill.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,7 @@ namespace TheSkyHomestay.Data.Models
         public Guid TouristId { get; set; }
         public DateTime CreatedTime { get; set; }
         public virtual User Tourist { get; set; }
+        public virtual List<RoomBooking> RoomBookings { get; set; }
+        public virtual List<ServiceBooking> ServiceBookings { get; set; }
     }
 }
