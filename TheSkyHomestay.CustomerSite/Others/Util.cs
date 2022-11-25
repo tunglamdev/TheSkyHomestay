2d0d047c3d6739cf9bf6e2dd465921d38df50f4d
Nguyen Tung Lam <ntlamctu@gmail.com>
2022-11-25 23:25:49 +0700

    Write component for payment with VNPAY

---
+++ TheSkyHomestay.CustomerSite/Others/Util.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TheSkyHomestay.CustomerSite.Others
+{
+    public class Util
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+        public Util(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public static String HmacSHA512(string key, String inputData)
+        {
+            var hash = new StringBuilder();
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
+            using (var hmac = new HMACSHA512(keyBytes))
+            {
+                byte[] hashValue = hmac.ComputeHash(inputBytes);
+                foreach (var theByte in hashValue)
+                {
+                    hash.Append(theByte.ToString("x2"));
+                }
+            }
+
+            return hash.ToString();
+        }
+
+        public static string GetIpAddress()
+        {
+            return "192.168.1.6";
+        }
+    }
+}
